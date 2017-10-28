19c5573edab36678a5b7e98f5e86867e5c6d3830
Alexander Droste <alexander.droste@progmem.de>
2017-10-28 19:24:21 +0200

    implemented user /create route (express)

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -7,26 +7,28 @@
 
 const express = require('express');
 const router = express.Router();
+const user = require('../controller/user');
 
 
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
     res.json({
         route: 'POST /login',
         body: req.body
     });
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', (req, res, next) => {
     res.json({
         route: 'POST /logout',
         body: req.body
     });
 });
 
-router.post('/create', (req, res) => {
-    res.json({
-        route: 'POST /create',
-        body: req.body
+router.post('/create', (req, res, next) => {
+    user.createUser(req.body['name'], req.body['email'], req.body['password'], (err, userEntry) => {
+        if (err)
+            return next(err);
+        return res.json(userEntry);
     });
 });
 
