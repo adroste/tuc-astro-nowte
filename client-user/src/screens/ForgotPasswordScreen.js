988da161b1c60710dbb57584247d38189c2adb2f
Felix Brüll <felixbruell@live.de>
2017-11-05 21:53:47 +0100

    added view for forgot password screen

--- client-user/src/screens/ForgotPasswordScreen.js
+++ client-user/src/screens/ForgotPasswordScreen.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import LabelledInputBox from "../ui/base/LabelledInputBox";
+import Button from "../ui/base/Button";
+import './UserForms.css';
+import LinkedText from "../ui/base/LinkedText";
+import {store} from "../Redux";
+import * as action from "../Actions"
 export default class ForgotPasswordScreen extends React.Component {
     /**
      * propTypes
@@ -14,10 +19,29 @@ export default class ForgotPasswordScreen extends React.Component {
         return {};
     }
 
+    handleLoginClick = () => {
+        store.dispatch(action.stateChange("login"));
+    };
+
+    handleResetClick = () => {
+        // TODO
+    };
+
     render() {
         return (
-            <div>
-                ForgotPasswordScreen
+            <div className="centered-form">
+                <div className="centered-form-inner">
+                    <LabelledInputBox
+                        label="Email"
+                        name="email"
+                        onClick={this.handleResetClick}
+                    />
+                    <br/>
+                    <Button label="Reset Password"/>
+                    <br/>
+                    <br/>
+                    Remembered? <LinkedText label="Log in" onClick={this.handleLoginClick}/>
+                </div>
             </div>
         );
     }
