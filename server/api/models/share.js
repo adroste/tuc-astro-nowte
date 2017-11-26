4e3cafbb5d18bcfa69c081bc6f4990cd72615b1e
Alexander Droste <alexander.droste@progmem.de>
2017-11-26 22:40:31 +0100

    #18 share scheme

---
+++ server/api/models/share.js
@@ -0,0 +1,36 @@
+/**
+ * @author progmem
+ * @date 26.11.17
+ */
+
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+
+const shareSchema = new Schema({
+    isFolder: {
+        type: Boolean,
+        required: [true, 'isFolder is required']
+    },
+    fileId: {
+        type: Schema.Types.ObjectId,
+        required: [true, 'fileId is required']
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'userId is required']
+    },
+    permission: {
+        type: Number,
+        required: [true, 'permission is required']
+    }
+});
+
+
+const Share = mongoose.model('Share', shareSchema);
+
+module.exports.Share = Share;
