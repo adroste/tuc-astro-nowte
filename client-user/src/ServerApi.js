9af17224232b4b5015fb008828a23cba2860dbc2
Felix Brüll <felixbruell@live.de>
2017-12-17 00:52:50 +0100

    added share api methods

--- client-user/src/ServerApi.js
+++ client-user/src/ServerApi.js
@@ -171,6 +171,40 @@ export const deleteProject = (projectId, onSuccess, onError) => {
     );
 };
 
+// see https://gitlab.progmem.de/tuc/astro-nowte/wikis/server/api/rest/file/set-access
+export const share = (projectId, email, permissions, onSuccess, onError) => {
+    const sessionToken = store.getState().user.token;
+    const url = SERVER_URL + '/api/file/set-access';
+
+    fetch(url, {
+        method: "PUT",
+        headers: REQUEST_HEADERS,
+        body: JSON.stringify({
+            sessionToken: sessionToken,
+            projectId: projectId,
+            shareEmail: email,
+            permissions: permissions,
+        })
+    }).then(
+        (response) => verifyResponseCode(response, 204, onSuccess, onError),
+        onError
+    );
+};
+
+// see https://gitlab.progmem.de/tuc/astro-nowte/wikis/server/api/rest/file/list-access
+export const getShares = (projectId, onSuccess, onError) => {
+    const sessionToken = store.getState().user.token;
+    const url = SERVER_URL + '/api/file/list-access/' + projectId + "?sessionToken=" + sessionToken;
+
+    fetch(url, {
+        method: "GET",
+        headers: REQUEST_HEADERS,
+    }).then(
+        (response) => getJsonBody(response, 200, onSuccess, onError),
+        onError
+    );
+};
+
 // helper to retrieve the json from a response
 const getJsonBody = (response, successStatusCode, onSuccess, onError) => {
     if(response.status === successStatusCode){
