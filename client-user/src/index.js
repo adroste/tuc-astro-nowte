77b6f70ccb0a6341d30d7a6095102e679a429065
Alexander Droste <alexander.droste@progmem.de>
2018-01-05 23:20:55 +0100

    save & reload state (localStorage, sessionStorage)

--- client-user/src/index.js
+++ client-user/src/index.js
@@ -7,6 +7,7 @@ import { routerReducer } from 'react-router-redux';
 import {project as projectReducer} from "./reducers/project";
 import {app as appReducer} from "./reducers/app";
 import {user as userReducer} from "./reducers/user";
+import {loadState, saveState} from "./utilities/storage";
 
 
 const reducers = combineReducers({
@@ -16,11 +17,27 @@ const reducers = combineReducers({
     app: appReducer,
 });
 
+const preloadedState = loadState();
+
 const store = createStore(
     reducers,
+    preloadedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+
+store.subscribe(() => {
+    const user = store.getState().user;
+    const project = store.getState().project;
+
+    saveState({
+        user: user
+    }, true);
+
+    saveState({
+        project: project
+    }, false);
+});
 
 
 
