bf3e5864fbcd772f25a397985e07f92345b3d579
Aaron Machmer <info@amachmer.de>
2018-01-12 18:42:46 +0100

    add basic dropdown menü component

---
+++ client-user/src/ui/base/DropdownMenu.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './DropdownMenu.css';
+
+export default class DropdownMenu extends React.Component {
+    /**
+     * propTypes
+     * @property {string} label for the Dropdownmenue
+     * @property {string} Array of entrys for the Dropdown
+     * @property {function()} onClick callback for openig dropdown
+     */
+    static get propTypes() {
+        return {
+            label: PropTypes.string,
+            entrys: PropTypes.strings,
+            onclick: PropTypes.func
+        };
+    }
+
+    static get defaultProps() {
+        return {
+
+        };
+    }
+
+    onClickHandler = (event) => {
+        if(this.props.onClick)
+            this.props.onClick();
+    };
+
+    render() {
+        return (
+            <div className="dropdown" onClick={this.onClickHandler}>
+                <button className="dropbtn">{this.props.label}</button>
+                <div className="dropdown-content">
+                    {this.props.label}
+                </div>
+            </div>
+        );
+    }
+}
