4e515db18e1cf4ba8a43e580e99f279c68e08d0a
Alexander Droste <alexander.droste@progmem.de>
2018-01-16 00:28:03 +0100

    created several Button themes

--- client-user/src/components/base/Button.js
+++ client-user/src/components/base/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import {COLOR_CODES} from "../../Globals";
 
 
 const Wrapper = styled.div`
@@ -41,39 +42,75 @@ const ButtonInner = styled.div`
 `;
 
 
-ButtonInner.defaultProps = {
-    theme: `
-        border: 1px solid #4CAF50;
+const createBorderTheme = (c1, c2, c3) => {
+    return `
+        border: 1px solid ${c1};
         border-radius: 5px;
-        color: #222;
+        color: ${c1};
+        transition: color 0.2s, border-color 0.2s;
         
         &:hover, 
         &:focus {
-            background-color: #4CAF50;
-            color: white;
+            border-color: ${c2};
+            color: ${c2};
         }
         
         &:active {
-            background-color: white;
-            color: black;
+            border-color: ${c3};
+            color: ${c3};
         }
-    `
+    `;
 };
+
+export const greenBorderTheme = createBorderTheme(COLOR_CODES.GREY, COLOR_CODES.GREEN, COLOR_CODES.GREEN_DARKER);
+export const greyBorderTheme = createBorderTheme(COLOR_CODES.GREY, COLOR_CODES.GREY_DARKER, COLOR_CODES.GREY_DARK);
+export const redBorderTheme = createBorderTheme(COLOR_CODES.GREY, COLOR_CODES.RED, COLOR_CODES.RED_DARKER);
+
+
+const createFilledTheme = (c1, c2, c3) => {
+    return `
+        background-color: ${c1};
+        border-radius: 5px;
+        color: white;
+        transition: background-color 0.2s;
+        
+        &:hover, 
+        &:focus {
+            background-color: ${c2};
+        }
+        
+        &:active {
+            background-color: ${c3};
+        }
+    `;
+};
+
+export const greenFilledTheme = createFilledTheme(COLOR_CODES.GREEN, COLOR_CODES.GREEN_LIGHTER, COLOR_CODES.GREEN_DARKER);
+export const redFilledTheme = createFilledTheme(COLOR_CODES.RED, COLOR_CODES.RED_LIGHTER, COLOR_CODES.RED_DARKER);
 
 
 export const lightGreyRoundedTheme = `
-    border: 1px solid #ccc;
+    border: 1px solid ${COLOR_CODES.GREY_LIGHT};
     border-radius: 15px;
-    color: #ccc;
-    transition: color 0.3s, border-color 0.3s;
+    color: ${COLOR_CODES.GREY_LIGHT};
+    transition: color 0.2s, border-color 0.2s;
     
     &:hover, 
     &:focus {
         background-color: #fff;
-        border-color: #777;
-        color: #777;
+        border-color: ${COLOR_CODES.GREY};
+        color: ${COLOR_CODES.GREY};
+    }
+    
+    &:active {
+        background-color: ${COLOR_CODES.GREY_DARKER};
     }
 `;
+
+
+ButtonInner.defaultProps = {
+    theme: greenBorderTheme
+};
 
 
 
@@ -81,7 +118,7 @@ export const lightGreyRoundedTheme = `
 export class Button extends React.Component {
     /**
      * propTypes
-     * @property {function()} onClick callback when button was clicked
+     * @property {function(event: Object)} onClick callback when button was clicked
      * @property {boolean} [center=false] if set button will be center inside block
      * @property {boolean} [large=false] if set text size will be bigger
      * @property {string} [theme] theme-template to apply
@@ -118,7 +155,7 @@ export class Button extends React.Component {
     }
 
     onClickHandler = (e) => {
-        this.props.onClick();
+        this.props.onClick(e);
         // TODO improve focusing
         this.ref.blur();
     };
