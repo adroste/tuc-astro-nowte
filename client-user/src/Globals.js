86abe1ba5fdf0ccf97e95ebbcd2f52d0631e31bf
Alexander Droste <alexander.droste@progmem.de>
2018-01-16 00:21:03 +0100

    global color-codes

--- client-user/src/Globals.js
+++ client-user/src/Globals.js
@@ -1,2 +1,18 @@
 
+
+export const COLOR_CODES = {
+    GREEN: '#4CAF50',
+    GREEN_LIGHTER: '#5CB65F',
+    GREEN_DARKER: '#46A049',
+    GREY_LIGHT: '#ddd',
+    GREY: '#777',
+    GREY_DARKER: '#555',
+    GREY_DARK: '#333',
+    RED: '#C75146',
+    RED_LIGHTER: '#CC6056',
+    RED_DARKER: '#AD2E24'
+
+};
+
+
 export const SERVER_URL = "http://localhost:3000";
