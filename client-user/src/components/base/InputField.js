0a1c281582b174070e334530a1755de64ba6ff40
Alexander Droste <alexander.droste@progmem.de>
2018-01-16 21:25:18 +0100

    InputField: themes & styles

--- client-user/src/components/base/InputField.js
+++ client-user/src/components/base/InputField.js
@@ -31,6 +31,15 @@ const LabelContainer = styled.div`
     text-align: left;
     font-size: ${FONT_SIZES.NORMAL};
     user-select: none;
+    
+    ${props => props.theme.label}
+`;
+
+
+const ChildrenContainer = styled.div`
+    font-size: ${FONT_SIZES.NORMAL};
+
+    ${props => props.theme.children}
 `;
 
 
@@ -40,15 +49,50 @@ const Input = styled.input`
     font-size: ${FONT_SIZES.BIGGER};
     padding: 10px;
     margin: 5px 0;
-    border: 1px solid ${COLOR_CODES.GREY_LIGHT};
     border-radius: 5px;
-    transition: border-color 0.2s;
     
     &:focus {
-        border-color: ${COLOR_CODES.GREY};
         outline: none;
     }
+    
+    ${props => props.theme.input}
 `;
+
+
+export const greyTheme = {
+    input: `
+        border: 1px solid ${COLOR_CODES.GREY_LIGHT};
+        transition: border-color 0.2s;
+        
+        &:focus {
+            border-color: ${COLOR_CODES.GREY};
+        }
+    `,
+    label: `
+        color: ${COLOR_CODES.GREY_DARK};
+    `,
+};
+
+
+export const errorTheme = {
+    input: `
+        border: 1px solid ${COLOR_CODES.RED_LIGHTER};
+        transition: border-color 0.2s;
+        
+        &:focus {
+            border-color: ${COLOR_CODES.GREY};
+        }
+    `,
+    label: `
+        color: ${COLOR_CODES.RED};
+    `,
+    children: `
+        color: ${COLOR_CODES.RED_DARKER};
+    `
+};
+
+
+// TODO green 'success' theme
 
 
 export class InputField extends React.Component {
@@ -57,20 +101,22 @@ export class InputField extends React.Component {
      * @property {string} [label] text above the input box
      * @property {string} [name] name of the input box
      * @property {string} value value (text) of the input box
-     * @property {function(text: string)} onChange callback when input box text was changed
+     * @property {function(text: string)} onInputChange callback when input box text was changed
      * @property {number} [maxLength=100] maximal character count
      * @property {string} [type=INPUT_TYPES.TEXT] type of the input box
      * @property {string} [placeholder] string that will be displayed in the background
+     * @property {object} [theme] theme-template to apply
      */
     static get propTypes() {
         return {
             label: PropTypes.string,
             name: PropTypes.string,
             value: PropTypes.string.isRequired,
-            onChange: PropTypes.func.isRequired,
+            onInputChange: PropTypes.func.isRequired,
             maxLength: PropTypes.number,
             type: PropTypes.string,
-            placeholder: PropTypes.string
+            placeholder: PropTypes.string,
+            theme: PropTypes.object
         };
     }
 
@@ -78,20 +124,23 @@ export class InputField extends React.Component {
         return {
             maxLength: 100,
             type: INPUT_TYPES.TEXT,
+            theme: greyTheme
         }
     }
 
 
     _onChangeHandler = (event) => {
-        this.props.onChange(event.target.value);
+        this.props.onInputChange(event.target.value);
     };
 
 
     render() {
         return (
-            <Wrapper>
+            <Wrapper className={this.props.className}>
                 {this.props.label &&
-                    <LabelContainer>
+                    <LabelContainer
+                        theme={this.props.theme}
+                    >
                         {this.props.label}
                     </LabelContainer>
                 }
@@ -102,10 +151,13 @@ export class InputField extends React.Component {
                     maxLength={this.props.maxLength}
                     value={this.props.value}
                     placeholder={this.props.placeholder}
+                    theme={this.props.theme}
                 />
-                <div>
+                <ChildrenContainer
+                    theme={this.props.theme}
+                >
                     {this.props.children}
-                </div>
+                </ChildrenContainer>
             </Wrapper>
         );
     }
