df611993c4d45867d165088b12860eb72fc3cd89
Alexander Droste <alexander.droste@progmem.de>
2018-01-17 03:05:22 +0100

    generic string empty validator, changed jsconfig.json

--- client-user/src/utilities/inputFieldValidators.js
+++ client-user/src/utilities/inputFieldValidators.js
@@ -3,35 +3,20 @@
  * @date 16.01.18
  */
 
-
-export const validateName = (name, cb) => {
-    if(name.length === 0){
-        return cb(false, 'Name field is required');
-    }
-
-    // TODO name requirements
+export const validateStringNotEmptyCustomMessage = (errorMessage) => (text, cb) => {
+    if(text.length === 0)
+        return cb(false, errorMessage);
     return cb(true, null);
 };
 
+// TODO name requirements
+export const validateName = validateStringNotEmptyCustomMessage('Name field is required');
 
-export const validateEmail = (email, cb) => {
-    if(email.length === 0){
-        return cb(false, 'Email field is required');
-    }
+// TODO verify email regex
+export const validateEmail = validateStringNotEmptyCustomMessage('Email field is required');
 
-    // TODO verify email regex
-    return cb(true, null);
-};
-
-
-export const validatePassword = (password, cb) => {
-    if(password.length === 0){
-        return cb(false, 'Password field is required');
-    }
-
-    // TODO password requirements
-    return cb(true, null);
-};
+// TODO password requirements
+export const validatePassword = validateStringNotEmptyCustomMessage('Password field is required');
 
 
 export const validatePasswordConfirm = (passwordToCompare) => (passwordConfirm, cb) => {
